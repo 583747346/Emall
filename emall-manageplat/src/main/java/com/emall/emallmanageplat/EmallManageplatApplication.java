984f9898bdb835a1602008ba5d05db3be2ebe7f1
qinlang <il>
2020-04-28 11:07:51 +0800

    build project

--- emall-manageplat/src/main/java/com/emall/emallmanageplat/EmallManageplatApplication.java
+++ emall-manageplat/src/main/java/com/emall/emallmanageplat/EmallManageplatApplication.java
@@ -1,9 +1,15 @@
 package com.emall.emallmanageplat;
 
+import com.alicp.jetcache.anno.config.EnableCreateCacheAnnotation;
+import com.alicp.jetcache.anno.config.EnableMethodCache;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableMethodCache(basePackages = "com.emall")
+@EnableCreateCacheAnnotation
 public class EmallManageplatApplication {
 
     public static void main(String[] args) {
