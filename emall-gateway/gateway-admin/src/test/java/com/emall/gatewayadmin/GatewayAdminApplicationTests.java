b050f81b695227ed372a2d9e07ce9845db908e94
qinlang <il>
2020-05-20 19:41:54 +0800

    build project

---
+++ emall-gateway/gateway-admin/src/test/java/com/emall/gatewayadmin/GatewayAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emall.gatewayadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
