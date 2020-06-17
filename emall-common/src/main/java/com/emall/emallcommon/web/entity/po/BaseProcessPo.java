859f90d21f440170374016f2453ed931d8e9ff0c
qinlang <il>
2020-06-17 14:19:32 +0800

    添加业务前端基类

---
+++ emall-common/src/main/java/com/emall/emallcommon/web/entity/po/BaseProcessPo.java
@@ -0,0 +1,25 @@
+package com.emall.emallcommon.web.entity.po;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+
+import java.util.Date;
+
+/**
+ * @author qinlang
+ * @des 购物车，订单等流程基类
+ */
+public class BaseProcessPo {
+
+    @TableId(type = IdType.ID_WORKER_STR)
+    private String id;
+
+    @TableField(fill = FieldFill.INSERT)
+    private Date createTime;
+
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Date updateTime;
+
+}
