ecc6794bd400bf8ed74439b6383507ca8336f081
qinlang <il>
2020-06-17 14:27:04 +0800

    商品评论模块

--- emall-manageplat/src/main/java/com/emall/emallmanageplat/web/ProductCommentController.java
+++ emall-manageplat/src/main/java/com/emall/emallmanageplat/web/ProductCommentController.java
@@ -5,7 +5,6 @@ import com.emall.emallmanageplat.entity.params.ProductCommentQueryParam;
 import com.emall.emallmanageplat.entity.vo.ProductCommentVo;
 import com.emall.emallmanageplat.service.IProductCommontService;
 import io.swagger.annotations.*;
-import org.checkerframework.checker.units.qual.A;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
