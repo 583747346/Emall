29c211bed0bc2ff0f4971311cf2b03ea9e2601e1
qinlang <il>
2020-06-24 16:32:08 +0800

    用户模块构建

--- emall-manageplat/src/main/java/com/emall/emallmanageplat/service/impl/UsersServiceImpl.java
+++ emall-manageplat/src/main/java/com/emall/emallmanageplat/service/impl/UsersServiceImpl.java
@@ -11,7 +11,6 @@ import com.emall.emallmanageplat.mapper.UsersMapper;
 import com.emall.emallmanageplat.service.IUsersService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
-import org.checkerframework.checker.units.qual.A;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
