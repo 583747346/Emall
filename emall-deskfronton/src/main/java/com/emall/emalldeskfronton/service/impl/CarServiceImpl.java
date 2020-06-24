c338e660db57ae961c5deac9cea0fe89041cbb4d
qinlang <il>
2020-06-24 17:44:37 +0800

    购物车模块

--- emall-deskfronton/src/main/java/com/emall/emalldeskfronton/service/impl/CarServiceImpl.java
+++ emall-deskfronton/src/main/java/com/emall/emalldeskfronton/service/impl/CarServiceImpl.java
@@ -2,6 +2,7 @@ package com.emall.emalldeskfronton.service.impl;
 
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.emall.emallcommon.web.entity.po.BasePo;
 import com.emall.emalldeskfronton.entity.params.CarQueryParam;
 import com.emall.emalldeskfronton.entity.po.EmallCarPo;
 import com.emall.emalldeskfronton.entity.vo.EmallCarVo;
@@ -14,5 +15,10 @@ public class CarServiceImpl extends ServiceImpl<CarMapper, EmallCarPo> implement
     @Override
     public IPage<EmallCarVo> getCars(CarQueryParam orderQueryParam) {
         return null;
+    }
+
+    @Override
+    public boolean insertCar(BasePo toPo) {
+        return false;
     }
 }
