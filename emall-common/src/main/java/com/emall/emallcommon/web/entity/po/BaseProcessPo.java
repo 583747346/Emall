9a516f69e2a4a32605eb43e759c3c501cf24d19f
qinlang <il>
2020-07-01 15:13:48 +0800

    rebuild

--- emall-common/src/main/java/com/emall/emallcommon/web/entity/po/BaseProcessPo.java
+++ emall-common/src/main/java/com/emall/emallcommon/web/entity/po/BaseProcessPo.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.Data;
 
 import java.util.Date;
 
@@ -11,6 +12,7 @@ import java.util.Date;
  * @author qinlang
  * @des 购物车，订单等流程基类
  */
+@Data
 public class BaseProcessPo {
 
     @TableId(type = IdType.ID_WORKER_STR)
