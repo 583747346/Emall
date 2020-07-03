8481884ac5a08d8cb8df4b6d5c3f6d993f27f3ee
qinlang <583747346@qq.com>
2020-07-03 15:43:38 +0800

    rebuild project

--- auth/authclient/src/main/java/com/emall/authclient/service/impl/AuthServiceImpl.java
+++ auth/authclient/src/main/java/com/emall/authclient/service/impl/AuthServiceImpl.java
@@ -2,7 +2,7 @@ package com.emall.authclient.service.impl;
 
 import com.emall.authclient.service.IAuthService;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
