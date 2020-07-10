63faf970f5889c04cb3efbb10fac64fe76df5426
qinlang <583747346@qq.com>
2020-07-10 11:14:47 +0800

    解决feign在调用的时候请求返回类型不一致的问题---HttpMessageConverters注入失败

---
+++ emall-gateway/gateway-web/src/main/java/com/emall/gatewayweb/config/FeignConfig.java
@@ -0,0 +1,49 @@
+package com.emall.gatewayweb.config;
+
+import feign.codec.Decoder;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.cloud.openfeign.support.ResponseEntityDecoder;
+import org.springframework.cloud.openfeign.support.SpringDecoder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 这里主要注入HttpMessageConverters到spring容器
+ * 由于请求协议不是很规范的情况下，需要配置Decoder
+ * 返回类型的不一致情况
+ */
+@Configuration
+public class FeignConfig {
+
+    @Bean
+    public Decoder feignDecoder() {
+        return new ResponseEntityDecoder(new SpringDecoder(feignHttpMessageConverter()));
+    }
+
+    public ObjectFactory<HttpMessageConverters> feignHttpMessageConverter() {
+        final HttpMessageConverters httpMessageConverters = new HttpMessageConverters(new MyMappingJackson2HttpMessageConverter());
+        return new ObjectFactory<HttpMessageConverters>() {
+            @Override
+            public HttpMessageConverters getObject() throws BeansException {
+                return httpMessageConverters;
+            }
+        };
+    }
+
+    public class MyMappingJackson2HttpMessageConverter extends MappingJackson2HttpMessageConverter {
+        MyMappingJackson2HttpMessageConverter(){
+            List<MediaType> mediaTypes = new ArrayList<>();
+            mediaTypes.add(MediaType.valueOf(MediaType.TEXT_HTML_VALUE + ";charset=UTF-8")); //关键
+            setSupportedMediaTypes(mediaTypes);
+        }
+    }
+
+
+}
