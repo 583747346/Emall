9c01c305fb9b1333d911fd8ab4256461dc8e1d03
qinlang <583747346@qq.com>
2020-07-13 12:28:38 +0800

    rebuild

---
+++ emall-common/emall-core/src/test/java/com/emall/emallcore/EmallCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emall.emallcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmallCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ emall-common/emall-web/src/test/java/com/emall/emallweb/EmallWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emall.emallweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmallWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
