0da11b29ac905e519f12cec9c46b58b36f7a22f2
qinlang <583747346@qq.com>
2020-07-16 00:42:47 +0800

    会员浏览记录控制器

---
+++ emall-deskfronton/src/main/java/com/emall/emalldeskfronton/web/MemberHistoryController.java
@@ -0,0 +1,14 @@
+package com.emall.emalldeskfronton.web;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 会员商品浏览记录
+ */
+@RestController
+@RequestMapping("/history")
+@Api(tags = "会员商品浏览记录",value = "MemberHistoryController")
+public class MemberHistoryController {
+}
