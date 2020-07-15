5b0a5431195af8910980c4286c735a98f013e8b3
qinlang <583747346@qq.com>
2020-07-16 00:40:00 +0800

    添加dao扫描，开启缓存注解等

--- emall-deskfronton/src/main/java/com/emall/emalldeskfronton/EmallDeskfrontonApplication.java
+++ emall-deskfronton/src/main/java/com/emall/emalldeskfronton/EmallDeskfrontonApplication.java
@@ -1,9 +1,17 @@
 package com.emall.emalldeskfronton;
 
+import com.alicp.jetcache.anno.config.EnableCreateCacheAnnotation;
+import com.alicp.jetcache.anno.config.EnableMethodCache;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@MapperScan("com.emall.emalldeskfronton.mapper")
+@EnableMethodCache(basePackages = "com.emall")
+@EnableCreateCacheAnnotation
 public class EmallDeskfrontonApplication {
 
     public static void main(String[] args) {
