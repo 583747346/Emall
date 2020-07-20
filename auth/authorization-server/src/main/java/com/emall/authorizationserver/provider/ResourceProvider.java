ab982795a4dd66d56a919300cf12678ec4f0ac0d
qinlang <583747346@qq.com>
2020-07-20 13:07:02 +0800

    注入bean

--- auth/authorization-server/src/main/java/com/emall/authorizationserver/provider/ResourceProvider.java
+++ auth/authorization-server/src/main/java/com/emall/authorizationserver/provider/ResourceProvider.java
@@ -3,12 +3,14 @@ package com.emall.authorizationserver.provider;
 import com.emall.authorizationserver.entity.Resource;
 import com.emall.emallcore.result.Result;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.Set;
 
 @FeignClient(name = "emall-manageplat", fallback = ResourceProviderFallback.class)
+@Component
 public interface ResourceProvider {
 
     @GetMapping(value = "/resource/user/{username}")
