a7e1e8978ae6a17ffcac3105197bbbeef9063aca
qinlang <583747346@qq.com>
2020-07-31 14:15:06 +0800

    rebuild

--- emall-manageplat/src/main/java/com/emall/emallmanageplat/web/MemberController.java
+++ emall-manageplat/src/main/java/com/emall/emallmanageplat/web/MemberController.java
@@ -12,6 +12,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 管理平台查看会员信息
+ */
 @RestController
 @Api(value = "MemberController", tags = "会员信息-API")
 @RequestMapping("/member")
