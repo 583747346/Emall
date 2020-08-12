17751af75c20951623c9d1af42b41fc62670a9ac
qinlang <583747346@qq.com>
2020-08-12 15:30:12 +0800

    rebuild

--- auth/authentication-server/src/main/java/com/emall/authenticationserver/provider/UserProvider.java
+++ auth/authentication-server/src/main/java/com/emall/authenticationserver/provider/UserProvider.java
@@ -5,18 +5,20 @@ import com.emall.authenticationserver.entity.Role;
 import com.emall.authenticationserver.entity.User;
 import com.emall.emallcore.result.Result;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Set;
 
+@Component
 @FeignClient(name = "emall-manageplat", fallback = UserProviderFallback.class)
 public interface UserProvider {
 
     @GetMapping(value = "/user")
-    Result<User> getUserByUniqueId(@RequestParam("uniqueId") String uniqueId);
+    Result<User> getUserByUniqueId(@RequestParam("uniqueId") Long uniqueId);
 
     @GetMapping(value = "/roles/user/{userId}")
-    Result<Set<Role>> queryRolesByUserId(@PathVariable("userId") String userId);
+    Result<Set<Role>> queryRolesByUserId(@PathVariable("userId") Long userId);
 }
