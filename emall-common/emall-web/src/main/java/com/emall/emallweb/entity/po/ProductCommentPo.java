2b147799bd03840bc9ff1675fba9ec6f2abdbbb2
qinlang <583747346@qq.com>
2020-08-28 17:11:22 +0800

    统一将所有实体类放到comment公共类

---
+++ emall-common/emall-web/src/main/java/com/emall/emallweb/entity/po/ProductCommentPo.java
@@ -0,0 +1,12 @@
+package com.emall.emallweb.entity.po;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.emall.emallweb.entity.po.BasePo;
+import io.swagger.annotations.ApiModel;
+import lombok.Data;
+
+@Data
+@TableName("emall_product_comment")
+@ApiModel("商品评论实体")
+public class ProductCommentPo extends BasePo {
+}
