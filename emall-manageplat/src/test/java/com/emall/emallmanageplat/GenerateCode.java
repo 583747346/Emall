a3379d23779a729a91d434b27726da013fd2fec6
qinlang <583747346@qq.com>
2021-03-02 22:20:05 +0800

    build

--- emall-manageplat/src/test/java/com/emall/emallmanageplat/GenerateCode.java
+++ emall-manageplat/src/test/java/com/emall/emallmanageplat/GenerateCode.java
@@ -8,62 +8,65 @@ import com.baomidou.mybatisplus.generator.config.*;
 import com.baomidou.mybatisplus.generator.config.po.TableInfo;
 import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
 import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
-import org.apache.commons.lang.StringUtils;
+import com.emall.emallweb.entity.po.BasePo;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class GenerateCode {
 
-    /**
-     * <p>
-     * 读取控制台内容
-     * </p>
-     */
-    public static String scanner(String tip) {
+    public String scanner(String tip) {
         Scanner scanner = new Scanner(System.in);
         StringBuilder help = new StringBuilder();
         help.append("请输入" + tip + "：");
         System.out.println(help.toString());
         if (scanner.hasNext()) {
             String ipt = scanner.next();
-            if (StringUtils.isNotEmpty(ipt)) {
+            if (StringUtils.isNotBlank(ipt)) {
                 return ipt;
             }
         }
         throw new MybatisPlusException("请输入正确的" + tip + "！");
     }
 
-    public static void main(String[] args) {
+    @Test
+    public void generate() throws NoSuchFieldException, IllegalAccessException {
         // 代码生成器
         AutoGenerator mpg = new AutoGenerator();
 
         // 全局配置
         GlobalConfig gc = new GlobalConfig();
         String projectPath = System.getProperty("user.dir");
-        gc.setOutputDir(projectPath + "/emall-manageplat/src/main/java");
+        gc.setOutputDir(projectPath + "/src/main/java");
+        //是否覆盖已有文件，默认false
+        //gc.setFileOverride(true);
         gc.setAuthor("qinlang");
         gc.setOpen(false);
-        gc.setSwagger2(true); //实体属性 Swagger2 注解
-        gc.setFileOverride(true);//设置是否覆盖原来的代码  最好设置为false  或者 另外配置路径
+        gc.setSwagger2(true);      //实体属性 Swagger2 注解
+        gc.setEntityName("%sPo");  // 设置实体类的命名
         mpg.setGlobalConfig(gc);
 
         // 数据源配置
         DataSourceConfig dsc = new DataSourceConfig();
-        dsc.setUrl("jdbc:mysql://139.196.160.33:3306/emall_manageplat?useUnicode=true&useSSL=false&characterEncoding=utf8");
+        dsc.setUrl("jdbc:mysql://139.196.160.33:3306/emall_manageplat?autoReconnect=true&useSSL=false&useUnicode=true&characterEncoding=utf8");
         // dsc.setSchemaName("public");
-        dsc.setDriverName("com.mysql.jdbc.Driver");
+        dsc.setDriverName("com.mysql.cj.jdbc.Driver");
         dsc.setUsername("root");
-        dsc.setPassword("123456");
+        dsc.setPassword("sm@123456");
         mpg.setDataSource(dsc);
 
         // 包配置
         PackageConfig pc = new PackageConfig();
-        pc.setModuleName(scanner("模块名"));
+//        pc.setModuleName(scanner("模块名"));
         pc.setParent("com.emall.emallmanageplat");
+        //自定义包路径
+        pc.setEntity("entity.po");
         mpg.setPackageInfo(pc);
-
         // 自定义配置
         InjectionConfig cfg = new InjectionConfig() {
             @Override
@@ -74,23 +77,17 @@ public class GenerateCode {
 
         // 如果模板引擎是 freemarker
         String templatePath = "/templates/mapper.xml.ftl";
-        // 如果模板引擎是 velocity
-        // String templatePath = "/templates/sys.xml.vm";
-
+//        // 如果模板引擎是 velocity
+//        // String templatePath = "/templates/mapper.xml.vm";
+//
         // 自定义输出配置
         List<FileOutConfig> focList = new ArrayList<>();
         // 自定义配置会被优先输出
         focList.add(new FileOutConfig(templatePath) {
-//            @Override
-//            public String outputFile(TableInfo tableInfo) {
-//                return null;
-//            }
-
             @Override
             public String outputFile(TableInfo tableInfo) {
                 // 自定义输出文件名 ， 如果你 Entity 设置了前后缀、此处注意 xml 的名称会跟着发生变化！！
-                return projectPath + "/src/main/resources/" + pc.getModuleName()
-                        + "/" + tableInfo.getEntityName() + "Mapper" + StringPool.DOT_XML;
+                return projectPath + "/src/main/resources/mapper/" + tableInfo.getEntityName() + "Mapper" + StringPool.DOT_XML;
             }
         });
         /*
@@ -100,7 +97,7 @@ public class GenerateCode {
                 // 判断自定义文件夹是否需要创建
                 checkDir("调用默认方法创建的目录，自定义目录用");
                 if (fileType == FileType.MAPPER) {
-                    // 已经生成 sys 文件判断存在，不想重新生成返回 false
+                    // 已经生成 mapper 文件判断存在，不想重新生成返回 false
                     return !new File(filePath).exists();
                 }
                 // 允许生成模板文件
@@ -118,27 +115,23 @@ public class GenerateCode {
         //指定自定义模板路径，注意不要带上.ftl/.vm, 会根据使用的模板引擎自动识别
         // templateConfig.setEntity("templates/entity2.java");
         // templateConfig.setService();
-        // templateConfig.setController();
+//        templateConfig.setController("web");
 
         templateConfig.setXml(null);
         mpg.setTemplate(templateConfig);
 
         // 策略配置
         StrategyConfig strategy = new StrategyConfig();
-        // 表名生成策略(下划线转驼峰命名)
         strategy.setNaming(NamingStrategy.underline_to_camel);
-        // 列名生成策略(下划线转驼峰命名)
         strategy.setColumnNaming(NamingStrategy.underline_to_camel);
-        // 自定义实体父类
-        // strategy.setSuperEntityClass("你自己的父类实体,没有就不用设置!");
-        // 是否启动Lombok配置
         strategy.setEntityLombokModel(true);
-        // 是否启动REST风格配置
         strategy.setRestControllerStyle(true);
         // 公共父类
-        // strategy.setSuperControllerClass("你自己的父类控制器,没有就不用设置!");
-        // 写于父类中的公共字段 父类没有id就注释掉，否则实体类不生成 id属性
-        // strategy.setSuperEntityColumns("id");
+//        strategy.setSuperControllerClass("");
+        // 写于父类中的公共字段
+        strategy.setSuperEntityClass(BasePo.class);
+        strategy.setSuperEntityColumns(new String[]{"id", "created_by", "created_time", "updated_by", "updated_time"});
+        //pms_full_promotion
         strategy.setInclude(scanner("表名，多个英文逗号分割").split(","));
         strategy.setControllerMappingHyphenStyle(true);
         strategy.setTablePrefix(pc.getModuleName() + "_");
--- emall-manageplat/src/test/java/com/emall/emallmanageplat/test.java
+++ emall-manageplat/src/test/java/com/emall/emallmanageplat/test.java
@@ -1,6 +1,9 @@
 package com.emall.emallmanageplat;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 public class test {
 
@@ -8,5 +11,9 @@ public class test {
 
         System.out.println(new Random().nextInt());
         System.out.println(System.getProperty("user.dir"));
+        String[] tables = {"pms_aaa_bbb", "oms_ccc_ddd", "ums_eee_rrr"};
+        List<String> collect = Arrays.stream(tables).map(table -> table.substring(table.indexOf('_') + 1, table.length())).collect(Collectors.toList());
+        System.out.println(collect.toArray(new String[collect.size()]));
+        System.out.println(String.format("%02d", 4654));
     }
 }
