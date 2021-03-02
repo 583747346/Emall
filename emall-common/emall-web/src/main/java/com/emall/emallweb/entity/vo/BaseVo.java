81385d98fb5d80b91a52c5587674288b71f7eaf5
qinlang <583747346@qq.com>
2021-03-02 22:36:53 +0800

    基础视图增加更新人与更新时间

--- emall-common/emall-web/src/main/java/com/emall/emallweb/entity/vo/BaseVo.java
+++ emall-common/emall-web/src/main/java/com/emall/emallweb/entity/vo/BaseVo.java
@@ -1,16 +1,27 @@
 package com.emall.emallweb.entity.vo;
 
 import com.emall.emallweb.entity.po.BasePo;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 import java.util.Date;
 
 @Data
 @NoArgsConstructor
 public class BaseVo<T extends BasePo> implements Serializable {
     private Long id;
+    @ApiModelProperty("创建人")
     private String createdBy;
-    private Date createdTime;
+    @ApiModelProperty("更新人")
+    private String updatedBy;
+    @ApiModelProperty("更新时间")
+    @JsonFormat( pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime updatedTime;
+    @ApiModelProperty("创建时间")
+    @JsonFormat( pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createdTime;
 }
