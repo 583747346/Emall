42ca061f8caea75b0cbe561bcab62dd1ba29bbf4
qinlang <583747346@qq.com>
2021-03-11 16:43:28 +0800

    库存模块

---
+++ emall-stock-service/src/main/java/com/emall/emallstockservice/EmallStockServiceApplication.java
@@ -0,0 +1,13 @@
+package com.emall.emallstockservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmallStockServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmallStockServiceApplication.class, args);
+    }
+
+}
---
+++ emall-stock-service/src/test/java/com/emall/emallstockservice/EmallStockServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emall.emallstockservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmallStockServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
