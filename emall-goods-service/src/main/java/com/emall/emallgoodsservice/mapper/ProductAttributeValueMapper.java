d81349ae82b82b9fde0ff857448088f61cea6d48
qinlang <583747346@qq.com>
2021-03-11 16:36:19 +0800

    商品属性规格值mapper接口层

---
+++ emall-goods-service/src/main/java/com/emall/emallgoodsservice/mapper/ProductAttributeValueMapper.java
@@ -0,0 +1,16 @@
+package com.emall.emallgoodsservice.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.emall.emallgoodsentity.entity.po.ProductAttributeValuePo;
+
+/**
+ * <p>
+ * 商品属性规格值 Mapper 接口
+ * </p>
+ *
+ * @author qinlang
+ * @since 2021-02-25
+ */
+public interface ProductAttributeValueMapper extends BaseMapper<ProductAttributeValuePo> {
+
+}
