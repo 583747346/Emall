ab481645efa6fc74f2faf15a12bd1083e92a0381
qinlang <583747346@qq.com>
2021-03-11 16:42:55 +0800

    营销模块

---
+++ emall-market-service/src/main/java/com/emall/emallmarketservice/EmallMarketServiceApplication.java
@@ -0,0 +1,13 @@
+package com.emall.emallmarketservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmallMarketServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmallMarketServiceApplication.class, args);
+    }
+
+}
---
+++ emall-market-service/src/test/java/com/emall/emallmarketservice/EmallMarketServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emall.emallmarketservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmallMarketServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
