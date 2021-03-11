47622ad9727e393794daf09f70504e9245541d28
qinlang <583747346@qq.com>
2021-03-11 16:34:03 +0800

    mybatis分页配置

---
+++ emall-goods-service/src/main/java/com/emall/emallgoodsservice/config/MybatisConfig.java
@@ -0,0 +1,21 @@
+package com.emall.emallgoodsservice.config;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+//开启事务管理器
+@EnableTransactionManagement
+@Configuration
+public class MybatisConfig {
+
+    /**
+     * 分页插件
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+
+}
