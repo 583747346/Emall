68bc7792262a47ff6a132a0fedbc9d1b6681c437
qinlang <583747346@qq.com>
2021-03-11 16:32:26 +0800

    商品分页查询参数增加删除状态条件

--- emall-entity-parent/emall-goods-entity/src/main/java/com/emall/emallgoodsentity/entity/params/ProductPageParam.java
+++ emall-entity-parent/emall-goods-entity/src/main/java/com/emall/emallgoodsentity/entity/params/ProductPageParam.java
@@ -11,14 +11,16 @@ public class ProductPageParam extends BasePageParam<ProductParam> {
 
     @ApiModelProperty(value = "产品名")
     private String name;
-    @ApiModelProperty(value = "产品类别")
-    private String productCategory;
+    @ApiModelProperty(value = "产品类别id")
+    private Long productCategoryId;
     @ApiModelProperty(value = "品牌")
-    private String brand;
+    private Long brandId;
     @ApiModelProperty(value = "促销类型,0->没有促销使用原价,1->使用促销价,2->使用会员价,4->使用满减价格")
     private Integer promotionType;
     @ApiModelProperty(value = "新品状态，0->新品,1->旧品")
     private Integer newStatus;
     @ApiModelProperty(value = "上线状态，0->上线,1->下线")
     private Integer publishStatus;
+    @ApiModelProperty(value = "删除状态，0->未删除,1->删除")
+    private Integer deleteStatus;
 }
